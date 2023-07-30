1a3fc70b091f7e6b774508e9984f63b50bb00ac2
Kingsley Atta Yeboah <114239382+kingsley805-tech@users.noreply.github.com>
2023-07-30 13:58:18 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      colors:{
+        VeryDarkMagenta:'hsl(300, 43%, 22%)',
+        SoftPink:'hsl(333, 80%, 67%)',
+        DarkGrayishMagenta:' hsl(303, 10%, 53%)',
+        LightGrayishMagenta:' hsl(300, 24%, 96%)',
+        White:' hsl(0, 0%, 100%)',
+      },
+  
+      
+    },
+  },
+  plugins: [],
+}
+
